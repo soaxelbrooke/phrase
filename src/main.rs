f680af0f6980c7cbd4279fec2638e80bcc4eef5a
Stuart Axelbrooke <stuart@axelbrooke.com>
2019-04-25 21:03:25 -0700

    change to using flat files instead of sqlite

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ extern crate rusqlite;
 extern crate env_logger;
 // extern crate native_stemmers; // see https://crates.io/crates/rust-stemmers
 
-use rocket_contrib::json::Json;
-use rusqlite::{Connection, params, NO_PARAMS};
+// use rocket_contrib::json::Json;
 use std::io::{BufReader, BufRead};
 use regex::Regex;
 use std::collections::{HashMap, VecDeque};
+use std::io::prelude::*;
+use std::io::LineWriter;
+use std::path::Path;
+use std::fs::File;
 
 const DB_PATH: &str = "database.sqlite";
 const PRUNE_AT: u32 = 1_000_000;
 const MAX_NGRAM: usize = 4;
+const FILE_DELIM: char = '\t';
 
 // URL regex
 // [-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
@@ -33,67 +37,9 @@ struct Document {
     body: String,
 }
 
-fn setup_database() -> rusqlite::Result<Connection> {
-    let conn = Connection::open(DB_PATH)?;
-
-    conn.pragma_update(None, "journal_mode", &"wal")?;
-
-    conn.execute(r#"
-        create table if not exists documents (
-            label text,
-            body text not null
-        );
-    "#, NO_PARAMS)?;
-
-    // TODO should we separate the phrase counting and the stem matching?
-    conn.execute(r#"
-        create table if not exists ngrams (
-            label text,
-            n integer not null,
-            ngram text not null,
-            ngram_stemmed text not null,
-            left_parent text,
-            right_parent text,
-            count integer not null,
-            document_count integer not null,
-            primary key (label, ngram, left_parent, right_parent)
-        );
-    "#, NO_PARAMS)?;
-
-    conn.execute(r#"
-        create table if not exists partition_stats (
-            label text,
-            head text not null,
-            position integer not null,
-            primary key (label, head)
-        );
-    "#, NO_PARAMS)?;
-
-    Ok(conn)
-}
-
-#[post("/documents", data = "<documents>")]
-fn create_documents(documents: Json<Vec<Document>>) -> String {
-    let mut conn = Connection::open(DB_PATH).expect("Couldn't open database conn.");
-    for document in &documents.0 {
-        if let Err(_err) = save_body_and_labels(&mut conn, &document.body, &document.labels) {
-            return format!("err: Unable to save document: {:?}", document);
-        }
-    }
-    String::from("ok")
-}
-
 #[get("/documents/statistics/<label>")]
 fn get_document_statistics(label: Option<String>) -> String {
-    let conn = Connection::open(DB_PATH).expect("Couldn't connect to database");
-    let mut stmt = conn.prepare(r#"
-        select 
-            count(*) as count
-        from documents
-        where label = ?
-    "#).unwrap();
-    let count: i64 = stmt.query_row(&[&label], |row| row.get(0)).unwrap();
-    format!("Total documents: {}", count)
+    format!("Total documents: {}", -1)
 }
 
 
@@ -102,156 +48,158 @@ fn hello() -> String {
     String::from("Hey there!")
 }
 
-fn save_body_and_labels(conn: &mut Connection, body: &String, labels: &Option<Vec<String>>) -> rusqlite::Result<()> {
-    if let Some(labels) = labels {
-        for label in labels {
-            save_body_and_label(conn, body, &Some(label))?;
-        }
-    } else {
-        save_body_and_label(conn, body, &None)?;
-    }
-    Ok(())
-}
-
-fn save_body_and_label(conn: &mut Connection, body: &String, label: &Option<&String>) -> rusqlite::Result<()> {
-    // let tx = conn.transaction()?;
-    // tx.execute(r#"insert into documents (body, label) values (?, ?)"#, params![body, label])?;
-    // let position = tx.last_insert_rowid();
-    // tx.execute(r#"
-    //     insert or replace into partition_stats (label, position, head) values ($1, $2, 'write');
-    // "#, params!(label, &position)).unwrap();
-    // tx.commit()?;
-    conn.execute(r#"insert into documents (body, label) values (?, ?)"#, params![body, label])?;
-    Ok(())
-}
-
-
 fn serve(port: u16) {
     let config = rocket::Config::build(rocket::config::Environment::Development)
         .port(port).finalize().expect("Couldn't create config.");
     rocket::custom(config)
-        .mount("/", routes![hello, create_documents, get_document_statistics])
+        .mount("/", routes![hello, get_document_statistics])
         .launch();
 }
 
-fn update_phrase_models(labels: Option<Vec<String>>) -> rusqlite::Result<()> {
+fn read_partition_counts_for_labels(labels: &Option<Vec<String>>, label_ngrams: &mut HashMap<Option<String>, HashMap<Vec<String>, i64>>) -> std::io::Result<()> {
     if let Some(labels) = labels {
         for label in labels {
-            update_phrase_model(Some(&label))?;
+            let ngrams = match read_partition_counts(Some(&label)) {
+                Ok(Some(ngrams)) => ngrams,
+                Ok(None) => HashMap::new(),
+                Err(err) => return Err(err),
+            };
+            label_ngrams.insert(Some(label.to_owned().to_owned()), ngrams);
         }
     } else {
-        update_phrase_model(None)?;
+        let ngrams = match read_partition_counts(None) {
+            Ok(Some(ngrams)) => ngrams,
+            Ok(None) => HashMap::new(),
+            Err(err) => return Err(err),
+        };
+        label_ngrams.insert(None, ngrams);
     }
     Ok(())
 }
 
-fn count_ngrams(body: &String, ngrams: &mut HashMap<usize, HashMap<VecDeque<String>, u64>>) {
-    // info!("Counting ngrams!");
+fn update_phrase_models(labels: Option<Vec<String>>, documents: Vec<String>) -> std::io::Result<()> {
+    let mut label_ngrams: HashMap<Option<String>, HashMap<Vec<String>, i64>> = HashMap::new();
+    read_partition_counts_for_labels(&labels, &mut label_ngrams)?;
+
+    if let Some(labels) = labels {
+        let counted_ngrams = count_ngrams(&documents);
+        for label in labels {
+            merge_ngrams_into(&counted_ngrams, label_ngrams.get_mut(&Some(label)).unwrap());
+        }
+    } else {
+        merge_ngrams_into(&count_ngrams(&documents), label_ngrams.get_mut(&None).unwrap());
+    }
+
+    for (label, ngrams) in label_ngrams {
+        write_partition_counts(label.as_ref(), &ngrams)?;
+    }
+
+    Ok(())
+}
+
+fn merge_ngrams_into(from: &HashMap<Vec<String>, i64>, into: &mut HashMap<Vec<String>, i64>) {
+    for (ngram, count) in from {
+        if let Some(into_count) = into.get_mut(ngram) {
+            *into_count += count;
+        } else {
+            into.insert(ngram.clone(), count.clone());
+        }
+    }
+}
+
+fn count_ngrams(documents: &Vec<String>) -> HashMap<Vec<String>, i64> {
+    let mut ngrams = HashMap::new();
+    info!("Counting ngrams for {} documents.", documents.len());
+
+    for document in documents {
+        count_document_ngrams(&document, &mut ngrams);
+    }
+
+    ngrams
+}
+
+fn count_document_ngrams(document: &String, ngrams: &mut HashMap<Vec<String>, i64>) {
     let re = Regex::new("[\\w']+").unwrap();
     let mut token_queues: Vec<VecDeque<String>> = Vec::new();
     for i in 1..MAX_NGRAM {
         token_queues.push(VecDeque::with_capacity(i));
     }
 
-    for token in re.find_iter(body) {
-        // if let Some(token) = token {
-            // info!("Processing token {}", token.as_str());
-            let token_string = token.as_str().to_string();
-            for i in 1..MAX_NGRAM {
-                if let Some(queue) = token_queues.get_mut(i - 1) {
-                    queue.push_back(token_string.to_owned());
-                    if queue.len() > i {
-                        queue.pop_front();
-                    }
-                    if queue.len() == i {
-                        if let Some(ngrams) = ngrams.get_mut(&i) {
-                            if let Some(count) = ngrams.get_mut(&queue) {
-                                *count += 1;
-                            } else {
-                                ngrams.insert(queue.to_owned(), 1);
-                            }
-                        }
-                    }
+    for token in re.find_iter(document) {
+        let token_string = token.as_str().to_string();
+        for i in 1..MAX_NGRAM {
+            if let Some(queue) = token_queues.get_mut(i - 1) {
+                queue.push_back(token_string.to_owned());
+                if queue.len() > i {
+                    queue.pop_front();
                 }
-            }
-        // }
-    }
-
-    // info!("Counted stuff: {:?}", ngrams);
-}
-
-fn ensure_read_head(conn: &mut Connection, label: Option<&String>) -> rusqlite::Result<usize> {
-    conn.execute(r#"
-        insert or replace into partition_stats (label, position, head) values (
-            $1,
-            coalesce((select position from partition_stats where label = $1 and head = 'read'), 0),
-            'read'
-        )
-    "#, params!(label))
-}
-
-fn update_phrase_model(label: Option<&String>) -> rusqlite::Result<()> {
-    info!("Updating phrase model for label {:?}.", &label);
-
-    let mut conn = Connection::open(DB_PATH)?;
-    let mut ngrams: HashMap<usize, HashMap<VecDeque<String>, u64>> = HashMap::new();
-
-    for i in 1..MAX_NGRAM {
-        ngrams.insert(i, HashMap::new());
-    }
-
-    info!("{:?}", label);
-
-    ensure_read_head(&mut conn, label)?;
-
-    let _res : Vec<rusqlite::Result<()>> = conn.prepare(r#"
-        select body from documents where label = $1 and rowid > (
-            select position from partition_stats where head = 'read' and label = $1
-        );
-    "#)?.query_map(params!(label), |row| {
-        Ok(count_ngrams(&row.get(0)?, &mut ngrams))
-    })?.collect();
-
-    for i in 1..MAX_NGRAM {
-        println!("{} len: {}", i, ngrams.get(&i).unwrap().len());
-    }
-
-    Ok(())
-}
-
-
-fn analyze_stdin(labels: Option<Vec<String>>) {
-    let mut conn = Connection::open(DB_PATH).expect("Unable to connect to database.");
-    let stdin = std::io::stdin();
-    let mut line = String::new();
-    loop {
-        match stdin.read_line(&mut line) {
-            Err(err) => {
-                error!("error: {}", err);
-                std::process::exit(1);
-            },
-            Ok(bytes) => {
-                if bytes == 0 {
-                    break;
-                } else {
-                    save_body_and_labels(&mut conn, &line, &labels).expect("Couldn't save line.");
+                if queue.len() == i {
+                    let queue: Vec<String> = queue.iter().map(|s| s.to_string()).collect();
+                    if let Some(count) = ngrams.get_mut(&queue) {
+                        *count += 1;
+                    } else {
+                        ngrams.insert(queue, 1);
+                    }
                 }
             }
         }
     }
+}
 
-    update_phrase_models(labels).expect("Failed to update phrase models");
+fn read_partition_counts(label: Option<&String>) -> std::io::Result<Option<HashMap<Vec<String>, i64>>> {
+    let path_str: String = match label {
+        Some(label) => format!("data/label={}", label),
+        None => String::from("data/root.txt"),
+    };
+    let path = Path::new(&path_str);
+    if path.exists() {
+        let file = File::open(path)?;
+        let file = BufReader::new(file);
+        let mut ngrams = HashMap::new();
+        for line in file.lines() {
+            if let Ok(line) = line {
+                let mut split = line.split(FILE_DELIM);
+                let ngram: Option<Vec<String>> = split.next().map(|s| s.split(' ').map(|s| s.to_string()).collect());
+                let count = split.next().map(|s| s.parse::<i64>());
+                if let Some(ngram) = ngram {
+                    if let Some(Ok(count)) = count {
+                        ngrams.insert(ngram, count);
+                    }
+                }
+            }
+        }
+
+        Ok(Some(ngrams))
+    } else {
+        Ok(None)
+    }
+}
+
+fn write_partition_counts(label: Option<&String>, ngrams: &HashMap<Vec<String>, i64>) -> std::io::Result<()> {
+    info!("Writing partition {:?}.", &label);
+    let file = match label {
+        Some(label) => File::create(format!("data/label={}.txt", label))?,
+        None => File::create("data/root.txt")?,
+    };
+    let mut file = LineWriter::new(file);
+    for (ngram, count) in ngrams {
+        let phrase = ngram.iter().map(|s| s.to_string()).collect::<Vec<String>>().join(" "); 
+        writeln!(file, "{}{}{}", phrase, FILE_DELIM, count)?;
+    }
+    file.flush()?;
+    Ok(())
+}
+
+fn analyze_stdin(labels: Option<Vec<String>>) {
+    let stdin = std::io::stdin();
+    let documents: Vec<String> = stdin.lock().lines().map(|s| s.expect("Coulnd't read line")).collect();
+    update_phrase_models(labels, documents).expect("Failed to update phrase models");
 }
 
 fn analyze_file(path: &str, labels: Option<Vec<String>>) {
     let file = std::fs::File::open(path).unwrap();
-    let mut conn = Connection::open(DB_PATH).expect("Unable to connect to database.");
-    for line in BufReader::new(file).lines() {
-        let line = line.expect("Couldn't read line from file.");
-        save_body_and_labels(&mut conn, &line, &labels).expect("Couldn't save line.");
-    }
-
-    update_phrase_models(labels).expect("Failed to update phrase models");
+    let documents: Vec<String> = BufReader::new(file).lines().map(|s| s.expect("Couldn't read line")).collect();
+    update_phrase_models(labels, documents).expect("Failed to update phrase models");
 }
 
 fn cmd_analyze(path: &str, labels: Option<Vec<String>>) {
@@ -278,15 +226,13 @@ fn main() {
         )
     ).get_matches();
 
-    setup_database().expect("Could not perform initial DB setup.");
-
     if let Some(matches) = matches.subcommand_matches("serve") {
         let port = matches.value_of("port").unwrap_or("6220").parse::<u16>().expect("Couldn't parse port.");
         serve(port);
     } else if let Some(matches) = matches.subcommand_matches("analyze") {
         env_logger::init();
         info!("Hello.");
-        let labels = matches.values_of("label").map(|values| values.map(String::from).collect());
+        let labels = matches.values_of("label").map(|values| values.map(|s| s.to_string()).collect());
         match matches.value_of("input") {
             Some(path) => {
                 cmd_analyze(path, labels);
