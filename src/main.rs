12569183ceaf58e765a9643ca6951a6e94d257c2
Stuart Axelbrooke <stuart@axelbrooke.com>
2019-05-02 19:21:25 -0700

    Promote to 0.3.0

--- src/main.rs
+++ src/main.rs
@@ -1074,7 +1074,7 @@ fn assert_label_valid(label: &Option<&String>) {
 
 fn main() {
     let matches = clap_app!(phrase => 
-        (version: "0.2.3")
+        (version: "0.3.0")
         (author: "Stuart Axelbrooke <stuart@axelbrooke.com>")
         (about: "Detect phrases in free text data.")
         (setting: clap::AppSettings::ArgRequiredElseHelp)
